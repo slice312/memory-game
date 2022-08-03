40daa129d850ad427847774bf7c0c5c9fd014796
Talgat <tairovtalgat@gmail.com>
2022-08-03 14:51:15 +0600

    return shuffling cards

--- src/pages/Game/model/data.js
+++ src/pages/Game/model/data.js
@@ -36,7 +36,7 @@ export const getCards = (mode) => {
     }
 
     return _.chain(cards)
-        // .shuffle() // TODO: вернуть
+        .shuffle()
         .map((card, i) => ({id: i, ...card}))
         .value();
 };
